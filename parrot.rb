d4ee69c8db89e14d37b8b05881aa44ee6c8c1cfe
Molly Seither <molly.seither@centre.edu>
2018-06-26 00:52:55 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string, out = "Squawk")
+def parrot(string="Squawk!")
   puts string
-    print out
+    return string
 end
